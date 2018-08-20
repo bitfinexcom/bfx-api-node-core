4020734432dfa7f92faaf6628b1f7ed7ad76c208
Cris Mihalache <me@f3rno.com>
2018-08-20 17:00:47 +0300

    (feature) plugin system, various refactorings to enable it

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict'
 
 const Manager = require('./lib/manager')
+const Config = require('./lib/config')
+const flags = require('./lib/ws2/flags')
 
 module.exports = {
   Manager,
+  Config,
+
+  ...flags,
 }
