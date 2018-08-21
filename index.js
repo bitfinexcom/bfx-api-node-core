db3a91be4e16c0669fa05672ae1e847241a7699b
Cris Mihalache <me@f3rno.com>
2018-08-21 13:41:46 +0300

    (refactor) add plugin state, helpers

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 const Manager = require('./lib/manager')
 const Config = require('./lib/config')
 const flags = require('./lib/ws2/flags')
+const plugins = require('./lib/plugins')
+const ws2 = require('./lib/ws2')
 
 module.exports = {
   Manager,
   Config,
 
   ...flags,
+  ...plugins,
+  ...ws2,
 }
