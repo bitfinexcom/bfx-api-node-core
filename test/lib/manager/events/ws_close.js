e5e455f969c5d0cd3a982a0a0dae60de8e47d4b1
Alexandru Dumitru <lecs.dumitru@gmail.com>
2020-01-30 23:32:21 +0000

    (test) add tests for manager events - ws_close

---
+++ test/lib/manager/events/ws_close.js
@@ -0,0 +1,30 @@
+/* eslint-env mocha */
+"use strict";
+
+const assert = require("assert");
+const wsClose = require("manager/events/ws_close");
+
+describe("manager:events:ws_close", () => {
+  it("emits selected ws id when ws found in wsPool", () => {
+    const wsId = 1000;
+    const manager = {
+      getWS: () => {},
+      notifyPlugins: (arg1, arg2, arg3, payload) => {
+        assert.strictEqual(payload.id === wsId);
+      },
+      emit: (eventName, payload) => assert.strictEqual(payload.id, wsId)
+    };
+
+    wsClose(manager, manager.id);
+  });
+
+  it("returns when ws not found in wsPool", () => {
+    const manager = {
+      getWS: () => void 0,
+      emit: () => assert.fail(),
+    };
+
+    const wsCloseReturnValue = wsClose(manager, manager.id);
+    assert.strictEqual(wsCloseReturnValue, undefined);
+  });
+});
