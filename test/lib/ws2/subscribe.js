b7bb86144ea3de876f90d3a7d453490c04b1c8d7
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-04-07 12:45:07 +0300

    test subscription

---
+++ test/lib/ws2/subscribe.js
@@ -0,0 +1,44 @@
+/* eslint-env mocha */
+'use strict'
+
+const assert = require('assert')
+const subscribe = require('../../../lib/ws2/subscribe')
+
+const defaultState = {
+  pendingSubscriptions: [],
+  channels: {},
+  sendBuffer: [],
+  emit: () => {}
+}
+
+describe('ws2:subscribe', () => {
+  it('has pending subscription', () => {
+    const nextState = subscribe(defaultState, 'book', {
+      prec: 'R0',
+      len: '25'
+    })
+    assert.deepStrictEqual(nextState.pendingSubscriptions, [[
+      'book', {
+        prec: 'R0',
+        len: '25'
+      }
+    ]])
+  })
+
+  it('prevent duplicate pending subscriptions', () => {
+    const nextState = subscribe(defaultState, 'book', {
+      prec: 'R0',
+      len: '25'
+    })
+    const nextState2 = subscribe(nextState, 'book', {
+      prec: 'R0',
+      len: '25'
+    })
+    assert.deepStrictEqual(nextState2.pendingSubscriptions, [[
+      'book', {
+        prec: 'R0',
+        len: '25'
+      }
+    ]])
+  })
+})
